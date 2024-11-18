7a8ec54b9902f8b4c74c004b0b30d14b64af414b
Miranda Callejón <mirandacallejonh@gmail.com>
2024-11-18 10:58:35 +0100

    limpieza de mapa antes de poner diferentes marcadores +  centrado y zoom a los marcadores existentes

--- src/app/components/map/map.component.ts
+++ src/app/components/map/map.component.ts
@@ -15,6 +15,8 @@ export class MapComponent implements OnInit {
   private userMarker: L.Marker<any> | undefined;
   theatreService = inject(TheatreService);
   listTheatres: Theatre[] = [];
+  markers: L.Marker[] = []; // Array para guardar referencias a los marcadores
+
 
 
   ngOnInit(): void {
@@ -31,6 +33,7 @@ export class MapComponent implements OnInit {
   }
 
   getLocation() {
+    this.cleanMap();
     if (navigator.geolocation) {
       navigator.geolocation.getCurrentPosition(position => {
         const coords: [number, number] = [position.coords.latitude, position.coords.longitude];
@@ -41,6 +44,8 @@ export class MapComponent implements OnInit {
           this.userMarker = L.marker(coords).addTo(this.map)
             .bindPopup('Estás aquí')
             .openPopup();
+          this.markers.push(this.userMarker);
+
         }
 
         this.map.setView(coords, 18)
@@ -54,16 +59,31 @@ export class MapComponent implements OnInit {
   }
 
   getTheatresLocations() {
+
+    this.cleanMap();
+    const bounds = L.latLngBounds([]); // Define límites para ajustar la vista del mapa
+
     this.theatreService.getListTheatres().subscribe((data: Theatre[]) => {
-      this.listTheatres = data; 
+      this.listTheatres = data;
       this.listTheatres.forEach(item => {
         if (item.latitude && item.longitude) {
           const marker = L.marker([item.latitude, item.longitude])
-          .addTo(this.map)
-          .bindPopup(`<b>${item.name}</b>`);
+            .addTo(this.map)
+            .bindPopup(`<b>${item.name}</b>`);
+          this.markers.push(marker);
+          bounds.extend([item.latitude, item.longitude]); // Amplía los límites con las coordenadas del marcador
         }
-      })     
+      })
+      if (bounds.isValid()) {
+        this.map.fitBounds(bounds, { padding: [50, 50] }); // Ajusta el mapa para incluir todos los marcadores
+      }
     })
   }
 
+  cleanMap() {
+    this.markers.forEach(marker => {
+      this.map.removeLayer(marker);
+    });
+    this.markers = [];
+  }
 }

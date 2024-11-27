54f4afe6639c6747b25ab9bb08bee803e267f6b8
Miranda Callejón <mirandacallejonh@gmail.com>
2024-11-27 10:55:28 +0100

    arreglado error deprecated import sass con bottstrap, cambio nombre full calendar a calendar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
 import { ChartjsComponent } from './components/chartjs/chartjs.component';
-import { FullCalendarComponent } from './components/full-calendar/full-calendar.component';
+import { CalendarComponent } from './components/calendar/calendar.component';
 import { MapComponent } from './components/map/map.component';
 import { AddEditComponent } from './components/add-edit/add-edit.component';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
     { path: 'graficos', component: ChartjsComponent },
-    { path: 'full-calendar', component: FullCalendarComponent },
+    { path: 'full-calendar', component: CalendarComponent },
     { path: 'map', component: MapComponent },
     { path: 'add', component: AddEditComponent },
     { path: 'edit/:id', component: AddEditComponent },
